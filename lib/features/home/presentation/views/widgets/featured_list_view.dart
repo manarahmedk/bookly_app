0c4aae3c3b1b9e854e9bea2f0f3c629056a9a13d
manar ahmed <manarahmedk@gmail.com>
2024-11-06 00:53:02 +0300

    add styles file and font family to app

--- lib/features/home/presentation/views/widgets/featured_list_view.dart
+++ lib/features/home/presentation/views/widgets/featured_list_view.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'custom_list_view_item.dart';
+import 'featured_list_view_item.dart';
 
 class FeaturedBooksListView extends StatelessWidget {
   const FeaturedBooksListView({super.key});
--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,6 +1,6 @@
+import 'package:bookly_app/core/utils/styles.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:flutter/material.dart';
-
 
 import 'featured_list_view.dart';
 
@@ -9,12 +9,22 @@ class HomeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  const Column(
-      children: [
-        CustomAppBar(),
-        FeaturedBooksListView(),
-      ],
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 24),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          CustomAppBar(),
+          FeaturedBooksListView(),
+          SizedBox(
+            height: 50,
+          ),
+          Text(
+            "Best Seller",
+            style: Styles.titleMedium,
+          ),
+        ],
+      ),
     );
   }
 }
-
