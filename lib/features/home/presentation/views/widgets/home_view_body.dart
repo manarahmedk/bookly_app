dd5b989f16db23d84589d5ad6465d82908cd3465
manar ahmed <manarahmedk@gmail.com>
2024-11-09 16:51:18 +0300

    add app styles

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -2,6 +2,7 @@ import 'package:bookly_app/core/utils/styles.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:flutter/material.dart';
 
+import '../../../../../core/utils/assrts.dart';
 import 'featured_list_view.dart';
 
 class HomeViewBody extends StatelessWidget {
@@ -21,10 +22,48 @@ class HomeViewBody extends StatelessWidget {
           ),
           Text(
             "Best Seller",
-            style: Styles.titleMedium,
+            style: Styles.textStyle18,
+          ),
+          SizedBox(
+            height: 25,
+          ),
+          BestSelllerListViewItem(),
+        ],
+      ),
+    );
+  }
+}
+
+class BestSelllerListViewItem extends StatelessWidget {
+  const BestSelllerListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 125,
+      child: Row(
+        children: [
+          AspectRatio(
+            aspectRatio: 2.5/4,
+            child: Container(
+              decoration:  BoxDecoration(
+                  borderRadius: BorderRadius.circular(8),
+                  image: const DecorationImage(
+                    fit: BoxFit.fill,
+                    image: AssetImage(
+                      AssetsData.testImage,
+                    ),
+                  )),
+            ),
+          ),
+          Column(
+            children: [
+
+            ],
           ),
         ],
       ),
     );
   }
 }
+
