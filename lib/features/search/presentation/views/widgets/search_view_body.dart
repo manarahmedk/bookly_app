9ed5956b47fb55b30afa0c03ae99721e2ba237b9
manar ahmed <manarahmedk@gmail.com>
2024-11-14 18:14:39 +0300

    add custom search text field

--- lib/features/search/presentation/views/widgets/search_view_body.dart
+++ lib/features/search/presentation/views/widgets/search_view_body.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
 
+import 'custom_search_text_feild.dart';
+
+
 class SearchViewBody extends StatelessWidget {
   const SearchViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 30),
+      child: Column(
+        children: [
+          CustomSearchTextField(),
+        ],
+      ),
+    );
   }
 }
+
+
