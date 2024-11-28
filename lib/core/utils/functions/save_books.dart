8cf300e951e065e2abfc1c10e9d6d0cb8126f290
manar ahmed <manarahmedk@gmail.com>
2024-11-29 02:04:29 +0300

    Cache featured books

---
+++ lib/core/utils/functions/save_books.dart
@@ -0,0 +1,10 @@
+import 'package:bookly_app/features/home/domain/entities/book_entity.dart';
+import 'package:hive_flutter/adapters.dart';
+
+void saveBooksData(List<BookEntity> books ,String boxName) {
+
+  var box = Hive.box(boxName);
+
+  box.add(books);
+
+}
--- lib/features/home/data/data_sources/home_remote_data_source.dart
+++ lib/features/home/data/data_sources/home_remote_data_source.dart
@@ -1,4 +1,6 @@
+import 'package:bookly_app/constants.dart';
 import 'package:bookly_app/core/utils/api_service.dart';
+import 'package:bookly_app/core/utils/functions/save_books.dart';
 import 'package:bookly_app/features/home/data/models/book_model/book_model.dart';
 import 'package:bookly_app/features/home/domain/entities/book_entity.dart';
 
@@ -22,8 +24,11 @@ class HomeRemoteDataSourceImpl extends HomeRemoteDataSource{
 
     List<BookEntity> books = getBooksList(data);
 
+    saveBooksData(books,featuredBox);
+
     return books;
   }
+
 
   @override
   Future<List<BookEntity>> fetchNewestBooks() async {
